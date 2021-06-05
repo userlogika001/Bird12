6f93b682ec490a6c130be87d61ab6ef80cb35484
Albatrosik <68684022+Albatrosik@users.noreply.github.com>
2021-06-05 12:19:57 +0300

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -1,1 +1,9 @@
+import pygame
+import random
+pygame.init()
 
+widthDisplay = 500
+heightDisplay = 500
+back = (200, 255, 255)
+mw = pygame.display.set_mode((widthDisplay, heightDisplay))
+mw.fill(back)
