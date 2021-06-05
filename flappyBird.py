d4094453062b692a9045618a78c6617b3c05d249
Maxon2005 <85391391+MaxTorgan@users.noreply.github.com>
2021-06-05 12:54:56 +0300

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -13,11 +13,16 @@ clock = pygame.time.Clock()
 
 game = True
 
-
-class label(Area):
-  def set_text((self, text, fsize=12,text_color=(0,0,0)):
-               self.image= pygame.font.SysFont('Areal',fsize).render(text,True,text_color)
-  def draw(self,shift_x=0,shift_y=0):
-               self.fill()
-               mw.blit(self.image,(self.rect.x+shift_x,self.rect.y+shift_y))
-               
+class Area():
+  def __init__(self,x=0,y=0,width=10,height=10,color=None):
+    self.rect = pygame.Rect(x,y,width,height)
+    self.fill_color = back
+    if color:
+      self.fill_color = color
+  def color(self,new color):
+  def fill(self):
+    pygame.draw.rect(mv,self.fill_color,self.rect)
+  def collidepoint(self,x,y):
+    return self.rect.collidepoint(x,y)
+  def colliderect(self,rect):
+    return self.rect.colliderect(rect)
