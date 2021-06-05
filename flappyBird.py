9c115eab86fd1781d75f084b259ddb5e0ad035e3
tpashen <81863630+tpashen@users.noreply.github.com>
2021-06-05 13:05:25 +0300

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -26,3 +26,9 @@ class Area():
     return self.rect.collidepoint(x,y)
   def colliderect(self,rect):
     return self.rect.colliderect(rect)
+ class Label(Area):
+    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+        self.image = pygame.font.SysFont('Arial', fsize).render(text, True, text_color)
+    def draw(self, shift_x=0, shift_y=0):
+        self.fill()
+        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
