86d4ff6bc269e65a8ba267b85f9b65f0c2efaa67
userlogika001 <84846485+userlogika001@users.noreply.github.com>
2021-06-05 12:38:28 +0300

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -12,3 +12,40 @@ clock = pygame.time.Clock()
 
 
 game = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#8
+if move_up:
+  bird.rect.y -=10
+for i in column_list:
+  if i.rect.colliderect(bird.rect):
+    game = False
+if bird.rect.y > 500 or bird.rect.y < 0:
+  game = False
+pygame.display.update()
+clock.tick(40)
