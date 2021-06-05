70c55e4b224ba8c98d27f427fbb4dd2f45602d6b
tpashen <81863630+tpashen@users.noreply.github.com>
2021-06-05 12:44:28 +0300

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -12,3 +12,12 @@ clock = pygame.time.Clock()
 
 
 game = True
+
+
+class label(Area):
+  def set_text((self, text, fsize=12,text_color=(0,0,0)):
+               self.image= pygame.font.SysFont('Areal',fsize).render(text,True,text_color)
+  def draw(self,shift_x=0,shift_y=0):
+               self.fill()
+               mw.blit(self.image,(self.rect.x+shift_x,self.rect.y+shift_y))
+               
